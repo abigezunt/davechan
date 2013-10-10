36b3ba918b503d5f96bfc1938abd55f620aa7867
Abby Howell <abby.g.howell@gmail.com>
2013-10-10 12:28:36 -0400

    update and delete now working

--- main.rb
+++ main.rb
@@ -10,3 +10,28 @@ end
 def query(sql)
 	CONN.exec(sql)
 end
+
+get '/davechan' do
+  @posts = query("SELECT * FROM davechan ORDER BY date")
+  erb :post_index
+end
+
+get '/davechan/:id' do
+  @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
+  erb :post_show
+end
+
+get '/davechan/:id/edit' do
+  @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
+  erb :post_edit
+end
+
+post '/davechan/:id/delete' do
+  query("DELETE FROM davechan WHERE id=#{params[:id]}")
+  redirect '/davechan'
+end
+
+post '/davechan/:id/update' do
+  query ("UPDATE davechan SET caption='#{params[:id]}', name='#{params[:name]}' WHERE id = #{params[:id]}")
+  redirect "/davechan/#{params[:id]}"
+end
