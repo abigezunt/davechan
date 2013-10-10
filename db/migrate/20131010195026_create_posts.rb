fa25e358cda37f386e612415df8bee3369d3b75a
Abby Howell <abby.g.howell@gmail.com>
2013-10-10 16:13:43 -0400

    migration complete

--- db/migrate/20131010195026_create_posts.rb
+++ db/migrate/20131010195026_create_posts.rb
@@ -1,13 +1,15 @@
 class CreatePosts < ActiveRecord::Migration
   def up
   	create_table :davechan do |t|
-  		t.string :url ||='http://placekitten.com/500/800'
-  		t.text :caption ||='so cute! omg!!'
-  		t.text :name ||='anonymous'
+  		t.text :url, default: "http://placekitten.com/500/800"
+  		t.text :caption, default: "so cute! omg!!"
+  		t.text :name, default: "anonymous"
   		t.timestamps
+  	end
   end
 
   def down
+  	drop_table :davechan
   end
 end
 
--- main.rb
+++ main.rb
@@ -2,41 +2,56 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
+require 'sinatra/activerecord'
 
-configure do |variable|
-	CONN = PG.connect(dbname: 'blog', host: 'localhost')
+set :database, {adapter: 'postgresql',
+                database: 'ga-class-posts',
+                host: 'localhost'}
+
+class Post < ActiveRecord::Base
 end
 
-def query(sql)
-	CONN.exec(sql)
-end
-
-get '/davechan' do
-  @posts = query("SELECT * FROM davechan ORDER BY date DESC")
+get '/' do
+  @posts = Post.all
+  binding.pry
   erb :post_index
 end
 
-post '/davechan/new' do
-  query("INSERT INTO davechan (url, caption, name) VALUES ('#{params[:url]}','#{params[:caption]}','#{params[:name]}')")
-  redirect '/davechan'
-end
 
-get '/davechan/:id' do
-  @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
-  erb :post_show
-end
+# configure do |variable|
+# 	CONN = PG.connect(dbname: 'blog', host: 'localhost')
+# end
 
-get '/davechan/:id/edit' do
-  @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
-  erb :post_edit
-end
+# def query(sql)
+# 	CONN.exec(sql)
+# end
 
-post '/davechan/:id/delete' do
-  query("DELETE FROM davechan WHERE id=#{params[:id]}")
-  redirect '/davechan'
-end
+# get '/davechan' do
+#   @posts = query("SELECT * FROM davechan ORDER BY date DESC")
+#   erb :post_index
+# end
 
-post '/davechan/:id/update' do
-  query ("UPDATE davechan SET caption='#{params[:id]}', name='#{params[:name]}' WHERE id = #{params[:id]}")
-  redirect "/davechan/#{params[:id]}"
-end
+# post '/davechan/new' do
+#   query("INSERT INTO davechan (url, caption, name) VALUES ('#{params[:url]}','#{params[:caption]}','#{params[:name]}')")
+#   redirect '/davechan'
+# end
+
+# get '/davechan/:id' do
+#   @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
+#   erb :post_show
+# end
+
+# get '/davechan/:id/edit' do
+#   @post = query("SELECT * FROM davechan WHERE id=#{params[:id]} LIMIT 1").first
+#   erb :post_edit
+# end
+
+# post '/davechan/:id/delete' do
+#   query("DELETE FROM davechan WHERE id=#{params[:id]}")
+#   redirect '/davechan'
+# end
+
+# post '/davechan/:id/update' do
+#   query ("UPDATE davechan SET caption='#{params[:id]}', name='#{params[:name]}' WHERE id = #{params[:id]}")
+#   redirect "/davechan/#{params[:id]}"
+# end
